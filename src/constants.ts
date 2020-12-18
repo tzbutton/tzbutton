22031b3b6be22d23d068f05286c59c7b194f9c9e
Pascal Brun <brunpasc@gmail.com>
2020-12-18 07:56:55 +0100

    feat(constants): xtz zo tez

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import { BigNumber } from 'bignumber.js'
 export const TZBUTTON_CONTRACT: string = 'KT1H28iie4mW9LmmJeYLjH6zkC8wwSmfHf5P'
 export const TZBUTTON_AMOUNT_MUTEZ: string = '200000'
 
-export const XTZ_STRING = 'XTZ'
+export const XTZ_STRING = 'tez'
 
 export const CONTRIBUTION_AMOUNT_STRING = (multiplier: number = 1) => {
   return `${new BigNumber(TZBUTTON_AMOUNT_MUTEZ)
