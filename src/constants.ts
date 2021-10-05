5c9d18328c08cdaae2dc2820b44ea9ce70f8fa65
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-10-05 10:27:33 +0200

    fix(rpc): use different rpc

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,12 @@
 import { BigNumber } from 'bignumber.js'
 
-export const NODE_URL = 'https://mainnet-tezos.giganode.io'
+export const NODE_URL = 'https://mainnet.api.tez.ie'
 
 export const TZBUTTON_CONTRACT: string = 'KT1H28iie4mW9LmmJeYLjH6zkC8wwSmfHf5P'
 export const TZBUTTON_AMOUNT_MUTEZ: string = '200000'
 
-export const TZBUTTON_COLOR_CONTRACT: string = 'KT1GFVf3ZVLiSxgU5EqBrmboEEU16prAdeJQ'
+export const TZBUTTON_COLOR_CONTRACT: string =
+  'KT1GFVf3ZVLiSxgU5EqBrmboEEU16prAdeJQ'
 export const TZCOLORS_CONTRACT: string = 'KT1FyaDqiMQWg7Exo7VUiXAgZbd2kCzo3d4s'
 
 export const XTZ_STRING = 'tez'
