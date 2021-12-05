1ff29bebce46c41baef3bdbaf95e5c51ea076cdf
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-12-05 06:31:25 +0100

    fix(rpc): update rpc

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from 'bignumber.js'
 
-export const NODE_URL = 'https://mainnet.api.tez.ie'
+export const NODE_URL = 'https://mainnet.smartpy.io'
 
 export const TZBUTTON_CONTRACT: string = 'KT1H28iie4mW9LmmJeYLjH6zkC8wwSmfHf5P'
 export const TZBUTTON_AMOUNT_MUTEZ: string = '200000'
